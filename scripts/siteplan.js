062cee4b3c84e16414229c6f72010b367014ebc1
siyabongamabuza25469 <mshengu25469@gmail.com>
2025-12-07 00:25:04 +0200

    changes

---
+++ scripts/siteplan.js
@@ -0,0 +1,4 @@
+let d = new Date();
+document.getElementById("currentYear").innerHTML = '&copy;' + d.getFullYear(); 
+document.querySelector('#lastModified').textContent = 'Last Modification: ' + document.lastModified; 
+
